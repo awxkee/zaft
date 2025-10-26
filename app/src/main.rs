02e8261335295b4c819c10ca111408f1e769ab7d
Radzivon Bartoshyk <radzivon.bartoshyk@proton.me>
2025-10-26 20:51:37 +0000

    AVX bf-7 improvement

--- app/src/main.rs
+++ app/src/main.rs
@@ -51,40 +51,40 @@ fn main() {
     // for (i, chunk) in data.iter_mut().enumerate() {
     //     *chunk = Complex::new(0.0019528865 + i as f64 * 0.1, 0.);
     // }
-    // 
+    //
     // let mut real_data = data.iter().map(|x| x.re).collect::<Vec<_>>();
     // let mut real_data_clone = real_data.to_vec();
     // let real_data_ref = real_data.clone();
-    // 
+    //
     // println!("real data {:?}", real_data);
-    // 
+    //
     // let forward_r2c = Zaft::make_r2c_fft_f64(data.len()).unwrap();
     // let inverse_r2c = Zaft::make_c2r_fft_f64(data.len()).unwrap();
-    // 
+    //
     // let mut complex_data = vec![Complex::<f64>::default(); data.len() / 2 + 1];
     // forward_r2c.execute(&real_data, &mut complex_data).unwrap();
     // // println!("r2c {:?}", complex_data);
     // inverse_r2c.execute(&complex_data, &mut real_data).unwrap();
-    // 
+    //
     // real_data = real_data
     //     .iter()
     //     .map(|&x| x * (1.0 / real_data.len() as f64))
     //     .collect();
-    // 
+    //
     // println!("c2r {:?}", real_data);
-    // 
+    //
     // let r_r2c = RealFftPlanner::new().plan_fft_forward(real_data.len());
     // let r_c2r = RealFftPlanner::new().plan_fft_inverse(real_data.len());
     // r_r2c
     //     .process(&mut real_data_clone, &mut complex_data)
     //     .unwrap();
     // r_c2r.process(&mut complex_data, &mut real_data).unwrap();
-    // 
+    //
     // real_data = real_data
     //     .iter()
     //     .map(|&x| x * (1.0 / real_data.len() as f64))
     //     .collect();
-    // 
+    //
     // real_data
     //     .iter()
     //     .zip(real_data_ref)
@@ -92,11 +92,11 @@ fn main() {
     //     .for_each(|(idx, (a, b))| {
     //         assert!((a - b).abs() < 1e-2, "a_re {}, b_re {} at {idx}", a, b);
     //     });
-    // 
+    //
     let o_data = data.clone();
-    // 
+    //
     let mut cvt = data.clone();
-    // 
+    //
     // let mut planner = FftPlanner::<f64>::new();
     //
     // for i in 1..1500 {
